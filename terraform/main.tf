302fcb66b4e50a52c7bc842b66befd3469b52403
khoil21 <khoil21@vt.edu>
2024-07-18 12:58:14 -0500

    tf main change: adding role

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,6 +31,27 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "example"
 }
