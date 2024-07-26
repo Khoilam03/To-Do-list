0a0675072abde3e9106b1c871bc7610489bd5663
khoil21 <khoil21@vt.edu>
2024-07-25 21:21:38 -0500

    terraform works

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,254 +1,44 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public_a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-}
-
-resource "aws_subnet" "public_b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-}
-
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-}
-
-resource "aws_route_table_association" "public_a" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_b" {
-  subnet_id      = aws_subnet.public_b.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.main.id
-  subnet_id     = aws_subnet.public_a.id
-}
-
-resource "aws_eip" "main" {
-  domain = "vpc"
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_security_group" "ecs_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+terraform {
+  backend "s3" {
+    bucket         = "todolistbucketmock"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "ccTfDemo"
+    encrypt        = true
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "alb_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_iam_role" "ecs_task_execution" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  ]
-
-  lifecycle {
-    ignore_changes = [name]
-  }
-}
-
-resource "aws_ecs_cluster" "main" {
-  name = "example"
-}
-
-resource "aws_lb" "main" {
-  name               = "example-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-
-  enable_deletion_protection = false
-
-  lifecycle {
-    ignore_changes = [name]
-  }
-}
-
-resource "aws_lb_target_group" "main" {
-  name        = "example-tg"
-  port        = 8000
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "ip"
-
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200"
-  }
-
-  lifecycle {
-    ignore_changes = [name]
-  }
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = "8000"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
-}
-
-resource "aws_cloudwatch_log_group" "example" {
-  name              = "/ecs/example"
-  retention_in_days = 7
-
-  lifecycle {
-    ignore_changes = [name]
-  }
-}
-
-resource "aws_ecs_task_definition" "main" {
-  family                   = "example"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-
-  container_definitions = jsonencode([
-    {
-      name  = "example"
-      image = var.ecr_repository_url
-      portMappings = [
-        {
-          containerPort = 8000
-          hostPort      = 8000
-        }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = aws_cloudwatch_log_group.example.name
-          awslogs-region        = var.aws_region
-          awslogs-stream-prefix = "ecs"
-        }
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
     }
-  ])
-}
-
-resource "aws_ecs_service" "main" {
-  name            = "example"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.main.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    assign_public_ip = true
-    subnets          = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-    security_groups  = [aws_security_group.ecs_sg.id]
   }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.main.arn
-    container_name   = "example"
-    container_port   = 8000
-  }
-
-  depends_on = [
-    aws_lb_listener.http
-  ]
 }
 
-variable "aws_region" {
-  description = "The AWS region to deploy to"
+module "tf-state" {
+  source      = "./modules/tf-state"
+  bucket_name = local.bucket_name
+  table_name  = local.table_name
 }
 
-variable "ecr_repository_url" {
-  description = "The URL of the ECR repository"
+module "ecrRepo" {
+  source = "./modules/ecr"
+
+  ecr_repo_name = local.ecr_repo_name
+}
+
+module "ecsCluster" {
+  source = "./modules/ecs"
+
+  demo_app_cluster_name = local.demo_app_cluster_name
+  availability_zones    = local.availability_zones
+
+  demo_app_task_famliy         = local.demo_app_task_famliy
+  ecr_repo_url                 = module.ecrRepo.repository_url
+  container_port               = local.container_port
+  demo_app_task_name           = local.demo_app_task_name
+  ecs_task_execution_role_name = local.ecs_task_execution_role_name
+
+  application_load_balancer_name = local.application_load_balancer_name
+  target_group_name              = local.target_group_name
+  demo_app_service_name          = local.demo_app_service_name
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,5 +1,5 @@
 aws_region           = "us-east-1"
-ecr_repository_url   = "471112851264.dkr.ecr.us-east-1.amazonaws.com/todolist:latest"
+ecr_repository_url   = "471112851264.dkr.ecr.us-east-1.amazonaws.com/todolist:7"
 # vpc_id               = "vpc-0ae3471ebbb67c232"
 # public_subnets       = ["subnet-00ab3810edf06a8c6"]
 # ecs_security_group_id = "sg-01e64399ed78d0cfc"
